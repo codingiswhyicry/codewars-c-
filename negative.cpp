3bdfd3e554f61a12c834fa514b7096cd9fc2f1a8
Amanda Southworth <amanda.southworth@myds.us>
2017-01-17 00:15:33 -0800

    added negative.coo

---
+++ negative.cpp
@@ -0,0 +1,12 @@
+/*
+    In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+ */
+
+int makeNegative(int num)
+{
+    int negative;
+    if (num > 0) {
+        negative = 0 - num;
+    }
+    return negative;
+}
